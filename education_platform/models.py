df06aa2696472c2ca96668c7c4ef753c59a28986
Sergo1613 <mentoss2012@gmail.com>
2024-04-28 23:17:48 +0300

    create Course and Lesson models

---
+++ education_platform/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Course(models.Model):
+    name = models.TextField(max_length=50, verbose_name='название курса')
+    preview = models.ImageField(upload_to='preview/', verbose_name='превью курса', **NULLABLE)
+    description = models.TextField(verbose_name='описание курса')
+    url = models.URLField(verbose_name='url', **NULLABLE)
+
+    class Meta:
+        verbose_name = 'курс'
+        verbose_name_plural = 'курсы'
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Lesson(models.Model):
+    name = models.TextField(max_length=50, verbose_name='название урока')
+    description = models.TextField(verbose_name='описание урока', **NULLABLE)
+    preview = models.ImageField(upload_to='preview/', verbose_name='превью урока', **NULLABLE)
+    video_link = models.URLField(verbose_name='ссылка на видео', **NULLABLE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, **NULLABLE, related_name='lessons')
+
+    class Meta:
+        verbose_name = 'урок'
+        verbose_name_plural = 'уроки'
+
+    def __str__(self):
+        return f'{self.name}'
+
